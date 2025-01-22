8ce4a19bc6ea1d13d96a0d476499d0cba844c6bb
Xasan Sodiqov <x.sodiqov@corp.jnq.uz>
2025-01-22 10:03:07 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib 
+import plotly.express as px  
+temp = pathlib.PosixPath   
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title('Transportni klassifikasiya qiluvchi model')
+
+#rasimni joylash 
+file = st.file_uploader('Rasimi joylsh', type=['png','svg', 'gif', 'jpeg'])
+if file:
+    #PIL convert
+    img = PILImage.create(file)
+
+    st.image(file)
+
+    # model
+    model = load_learner('transport_model.pkl')
+
+    #prediction
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(f'Bashorat: {pred}')
+    st.info(f'Extimollik: {probs[pred_id]*100: .1f}%')
+
+    #ploting
+fig = px.bar(x=probs*100, y=model.dls.vocab)
+st.plotly_chart(fig)
